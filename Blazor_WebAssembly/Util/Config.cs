b4685de484356c6857b23fd6999f559cd5bf4fd9
Maurilio Medeiros <maurilioalm@hotmail.com>
2021-06-07 13:38:44 -0300

    Componentes em castata, envio de parâmetros.

---
+++ Blazor_WebAssembly/Util/Config.cs
@@ -0,0 +1,8 @@
+﻿namespace Blazor_WebAssembly.Util
+{
+    public class Config
+    {
+        public string Cor { get; set; } = "black";
+        public string Tamanho { get; set; } = "10px";
+    }
+}
