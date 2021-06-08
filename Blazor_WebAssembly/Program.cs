71d58126d954930a86dc20493542f17d1f578c9a
Maurilio Medeiros <maurilioalm@hotmail.com>
2021-06-08 17:56:58 -0300

    Tarefas, parâmetros, EventCallback, componente como referência.

--- Blazor_WebAssembly/Program.cs
+++ Blazor_WebAssembly/Program.cs
@@ -1,3 +1,4 @@
+using Blazor_Tarefas.Repositorios;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -14,6 +15,8 @@ namespace Blazor_WebAssembly
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddScoped<IRepositorio, Repositorio>();
 
             await builder.Build().RunAsync();
         }
---
+++ Blazor_WebAssembly/wwwroot/JavaScriptFunctions.js
@@ -0,0 +1,17 @@
+﻿window.MostrarAlerta = (message) => {
+    alert(message);
+}
+
+function getTotalTarefasInstancia(dotnet) {
+    return dotnet.invokeMethodAsync("ObterTotalTarefasInstancia")
+        .then(resultado => {
+            alert("Total de tarefas: " + resultado);
+        });
+}
+
+function getTotalTarefas() {
+    Dotnet.invokeMethodAsync("Blazor_WebAssembly", "ObterTotalTarefas")
+        .then(resultado => {
+            alert("Total de tarefas: " + resultado);
+        });
+}
