618479de7d3ff79f0b8138c7756bed03ff498102
Maurilio Medeiros <maurilioalm@hotmail.com>
2021-06-10 14:53:21 -0300

    Validation Message.

--- Blazor_WebAssembly/Model/Usuario.cs
+++ Blazor_WebAssembly/Model/Usuario.cs
@@ -6,13 +6,16 @@ namespace Blazor_WebAssembly.Model
     {
         public int Id { get; set; }
 
-        [Required]
+        //Dois Atibutos de validação.
+        [Required(ErrorMessage = "O nome deve ser informado.")]
+        [StringLength(10, ErrorMessage = "O nome não pode exeder 10 caracteres.")]
         public string Nome { get; set; }
 
-        [EmailAddress]
+        [Required(ErrorMessage = "O email deve ser informado.")]
+        [EmailAddress(ErrorMessage = "Formato do email inválido.")]
         public string Email { get; set; }
 
-        [Range(18, 80, ErrorMessage = "A idade deve estar entre 18 e 80 anos")]
+        [Range(18, 80, ErrorMessage = "A idade deve estar entre 18 e 80 anos.")]
         public int Idade { get; set; }
     }
 }
