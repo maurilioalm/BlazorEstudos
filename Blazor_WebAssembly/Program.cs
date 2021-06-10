2dce92f9a963ddf479d6a3fbfba79f429fc1c21d
Maurilio Medeiros <maurilioalm@hotmail.com>
2021-06-10 11:50:30 -0300

    NavegationManager, NavegationTo.

--- Blazor_WebAssembly/Program.cs
+++ Blazor_WebAssembly/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Blazored.LocalStorage;
 
 namespace Blazor_WebAssembly
 {
@@ -17,6 +18,10 @@ namespace Blazor_WebAssembly
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             builder.Services.AddScoped<IRepositorio, Repositorio>();
+
+            //Servico de Local Storage
+            builder.Services.AddBlazoredLocalStorage();
+
 
             await builder.Build().RunAsync();
         }
