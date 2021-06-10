25dc35f50fa6210ad18b86c8f7e472bea2382a04
Maurilio Medeiros <maurilioalm@hotmail.com>
2021-06-10 14:25:02 -0300

    Implementada as validações nos campos do formulário. DataAnnotations.

--- Blazor_WebAssembly/Model/Usuario.cs
+++ Blazor_WebAssembly/Model/Usuario.cs
@@ -1,10 +1,18 @@
-﻿namespace Blazor_WebAssembly.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blazor_WebAssembly.Model
 {
     public class Usuario
     {
         public int Id { get; set; }
+
+        [Required]
         public string Nome { get; set; }
+
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Range(18, 80, ErrorMessage = "A idade deve estar entre 18 e 80 anos")]
         public int Idade { get; set; }
     }
 }
